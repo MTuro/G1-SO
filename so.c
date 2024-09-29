3b4be6f2f693013243d3ed324fbbcb23c7771b66
MTuro <marcosturo69@gmail.com>
2024-09-29 19:38:16 -0300

    nada pronto ainda

--- so.c
+++ so.c
@@ -5,14 +5,16 @@
 #include <sys/wait.h>
 #include <time.h>
 
-#define NUM_PROCESSES 3 // num de processos
+#define NUM_PROCESSES 3     // num de processos
 #define MAX_ITER 10        // maximo de interacoes dos procesos
-#define TIME_SLICE 500000 // 500ms (usleep usa microsegundos) 
+#define TIME_SLICE 0.5    // 500ms
 
-pid_t processes[NUM_PROCESSES];     // array de Pids
+pid_t processes[NUM_PROCESSES];      // array de Pids
 int program_counter[NUM_PROCESSES]; // contador cada processo
-int current_process = 0;            // processo em execucao
-
+int current_process = 0;           // processo em execucao
+int pipes[2];                     // descritor de pipes
+int process_blocked[NUM_PROCESSES];
+int device_blocked[2];
 
 void sigcont_handler(int signum) {
     printf("Processo %d: Retomando execução\n", getpid());
@@ -23,18 +25,41 @@ void sigstop_handler(int signum) {
     pause();  // processo esperando retomada 
 }
 
+void syscall_sim(int device, int process_id, char operation) {
+    if (operation == 'R') {
+        char result;
+        read(pipes[0], result, sizeof(char));
+    }
+    else if (operation == 'W') {
+        write(pipes[1], '0', sizeof(char));
+    }
+    else {
+        //nem sei oq eh X
+    }
+    process_blocked[process_id] = 1; // Bloqueia o processo
+    device_blocked[process_id] = device;
+    printf("Processo %d bloqueado no dispositivo D%d\n", process_id, device);
+}
+
 void process_application(int id) {
-    signal(SIGCONT, sigcont_handler); // Define o handler para SIGCONT
-    signal(SIGSTOP, sigstop_handler); // Define o handler para SIGSTOP
+    int device, operation;
+    int chance;
 
     while (program_counter[id] < MAX_ITER) {
         printf("Processo %d: Executando, PC=%d\n", id, program_counter[id]);
         program_counter[id]++;
-        usleep(100000); // Simulação de trabalho (100ms por iteração)
+        sleep(1); // Simulação de trabalho (100ms por iteração)
 
-        if (rand() % 100 < 15) {
-             // Simula chance de fazer uma syscall (15% de chance)
+        if (chance == rand() % 100 < 15) {  // Simula chance de fazer uma syscall (15% de chance)
+            if (chance % 2) device = 1;
+            else device = 2;
+            if (chance % 3 == 1) operation = 'R';
+            else if (chance % 3 == 1) operation = 'W';
+            else operation = 'X';
+            syscall_sim(device, id, operation);
+            
             printf("Processo %d: Realizando syscall, entrando em espera...\n", id);
+            kill(getpid(), SIGSTOP);
             pause(); // Simula bloqueio por syscall
         }
     }
@@ -44,6 +69,9 @@ void process_application(int id) {
 }
 
 void create_processes() {
+    signal(SIGCONT, sigcont_handler); // Define o handler para SIGCONT
+    signal(SIGSTOP, sigstop_handler); // Define o handler para SIGSTOP
+
     for (int i = 0; i < NUM_PROCESSES; i++) {
         program_counter[i] = 0; 
         processes[i] = fork();   // Cria o processo filho
@@ -56,13 +84,13 @@ void create_processes() {
 }
 
 void kernel_sim() {
-    struct timespec ts = {0, TIME_SLICE}; // Time slice de 500ms
+    create_processes(); // Cria os processos de aplicação 
 
-    while (1) {
+    while(1) {
         printf("Kernel: Executando processo %d\n", current_process);
         kill(processes[current_process], SIGCONT); // Continua o processo atual
 
-        nanosleep(&ts, NULL); // Espera o time slice (500ms)
+        sleep(TIME_SLICE); // Espera o time slice (500ms)
 
         // Pausa o processo atual e seleciona o próximo
         kill(processes[current_process], SIGSTOP); // Pausa o processo
@@ -70,10 +98,32 @@ void kernel_sim() {
     }
 }
 
+void controller_sim(){
+    while(1){
+        
+    }
+}
+
 int main() {
-    srand(time(NULL)); // Inicializa o gerador de números aleatórios
-    create_processes(); // Cria os processos de aplicação
-    kernel_sim();       // Inicia a simulação do Kernel
+    if (pipe(pipes) < 0) {
+        puts("error creating pipe");
+        exit(1);
+    }
+
+    pid_t kernel_pid = fork(); // criando o processo do InterController
+
+    if (kernel_pid == 0) {
+        kernel_sim();  // Inicia a simulação do Kernel
+    }
+    else {
+        pid_t ICS_pid = fork();
+
+        if (ICS_pid == 0) {
+            controller_sim();
+        }
+    }
+
+    // srand(time(NULL));   // Inicializa o gerador de números aleatórios
 
     return 0;
 }
