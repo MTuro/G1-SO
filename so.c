75a69ee7f3224c727d435c0376a5f2fc5a267e62
luiza <luiza@anaa.myguest.virtualbox.org>
2024-10-05 19:26:00 -0300

    bruxarias acontecendo

--- so.c
+++ so.c
@@ -23,11 +23,9 @@ boa noite
 
 int blocked_process[NUM_DEVICES][NUM_PROCESSES]; // fila de processos bloqueados
 pid_t processes[NUM_PROCESSES];                 // array de PIDs
-int program_counter[NUM_PROCESSES];            // contador cada processo
+int program_counter[MAX_ITER];            // contador cada processo
 int *current_process;                         // processo em execucao (memoria compartilhada)
 int pipes[NUM_PROCESSES][2];                 // descritor de pipes para comunicação
-__sighandler_t cont;
-__sighandler_t stop;
 
 void D1_handler(int signum) {
     for (int i = 0; i < NUM_PROCESSES; i++) {
@@ -57,32 +55,29 @@ void TS_handler(int signum) {
 
 void sigcont_handler(int signum) {
     printf("Processo %d: Retomando execução\n", *current_process);
-    cont(signum);
 }
 
 void sigstop_handler(int signum) {
     printf("Processo %d: Pausado pelo Kernel\n", *current_process);
-    stop(signum);
 }
 
 void syscall_sim(int device, char* operation) {
     /*
         Simula uma chamada para o kernel de término de processo
     */
-    printf("Dispositivo D%d: Solicitação de %s \n", device, operation);
+    printf("Dispositivo D%d: Solicitação de %s \n", device+1, operation);
     if (device == 0) {
-        kill(*current_process, SIGUSR1);
+        kill(processes[*current_process], SIGUSR1);
     } 
     else if (device == 1) {
-        kill(*current_process, SIGUSR2);
+        kill(processes[*current_process], SIGUSR2);
     }
-    printf("Processo %d bloqueado no dispositivo D%d\n", *current_process, device);
+    printf("Processo %d bloqueado no dispositivo D%d\n", *current_process, device+1);
 }
 
 void process_application(int id) {
-    cont = signal(SIGCONT, sigcont_handler); // Define o handler para SIGCONT
-    pause();
-    stop = signal(SIGSTOP, sigstop_handler); // Define o handler para SIGSTOP
+    signal(SIGCONT, sigcont_handler); // Define o handler para SIGCONT
+    signal(SIGSTOP, sigstop_handler); // Define o handler para SIGSTOP
     int device, chance;
     char* operation;
 
@@ -104,7 +99,6 @@ void process_application(int id) {
             blocked_process[device][id] = 1;
             raise(SIGSTOP);
         }
-
     }
     printf("Processo %d: Finalizado\n", id);
     exit(0);
@@ -130,6 +124,9 @@ void kernel_sim() {
         ou o sinal de aviso do término de sua fatia de tempo
     */
     create_processes(); // Cria os processos de aplicação
+    for (int i = 0; i < NUM_PROCESSES; i++){
+        kill(processes[i], SIGSTOP);
+    }
 
     while (1) {
         // Verifica se todos os processos já terminaram
@@ -148,8 +145,8 @@ void kernel_sim() {
 
         printf("Kernel: Executando processo %d\n", *current_process);
         kill(processes[*current_process], SIGCONT); // Retoma a execução do processo atual
-        sleep(TIME_SLICE);
 
+        sleep(TIME_SLICE);
         // Após o time slice, pausa o processo
         kill(processes[*current_process], SIGSTOP); // Pausa o processo
         printf("Kernel: Pausando processo %d\n", *current_process);
@@ -170,16 +167,16 @@ void controller_sim() {
     */
     while (1) {
         sleep(TIME_SLICE); // IRQ0 a cada 500ms
-        kill(*current_process, SIGRTMIN); // Envia IRQ0 (time slice)
+        kill(processes[*current_process], SIGRTMIN); // Envia IRQ0 (time slice)
 
         // IRQ1 tem probabilidade de 0.1
         if ((rand() % 100) < 10) {
-            kill(*current_process, SIGUSR1); // Envia IRQ1 (D1)
+            kill(processes[*current_process], SIGUSR1); // Envia IRQ1 (D1)
         }
 
         // IRQ2 tem probabilidade de 0.05
         if ((rand() % 100) < 5) {
-            kill(*current_process, SIGUSR2);  // Envia IRQ2 (D2)
+            kill(processes[*current_process], SIGUSR2);  // Envia IRQ2 (D2)
         }
     }
 }
@@ -193,16 +190,25 @@ int main() {
     current_process = (int *)shmat(shm_id, NULL, 0);
     *current_process = 0;  // Inicializa o processo atual
 
+    int shm_program[NUM_PROCESSES];
+
+    // for (int i = 0; i < NUM_PROCESSES; i++){
+    //     shm_program[i] = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
+    //     program_counter[i] = (int *)shmat(shm_program[i], NULL, 0);
+    //     *program_counter[i] = 0;
+    // }
+
+
     pid_t ICS_pid = fork();
 
     if (ICS_pid == 0) {
         controller_sim(); // Inicia a simulação do InterController
-    } else {
-        pid_t kernel_pid = fork();
+    } 
 
-        if (kernel_pid == 0) {
-            kernel_sim(); // Inicia o kernel de escalonamento
-        }
+    pid_t kernel_pid = fork();
+
+    if (kernel_pid == 0) {
+        kernel_sim(); // Inicia o kernel de escalonamento
     }
 
     // Aguarda o término dos processos filhos
@@ -210,6 +216,11 @@ int main() {
 
     shmdt(current_process);  // Desanexa a memória compartilhada
     shmctl(shm_id, IPC_RMID, NULL);  // Remove o segmento de memória compartilhada
+
+    // for (int i = 0; i < NUM_PROCESSES; i ++){
+    //     shmdt(program_counter[i]);  // Desanexa a memória compartilhada
+    //     shmctl(shm_program[i], IPC_RMID, NULL);  // Remove o segmento de memória compartilhada
+    // }
 
     return 0;
 }
